11bd86dbf53eef328d6b609a0780c1a6778069cd
Miguel Colque <micky.cm@outlook.es>
2025-12-04 18:25:54 -0400

    Ejercicios de estilar la página de details y programar los filtros de tecnología y ubicación

--- 00-html-css/script.js
+++ 00-html-css/script.js
@@ -17,8 +17,54 @@ jobListingSection?.addEventListener('click', (event) => {
   }
 })
 
-const filterSelect = document.querySelector('#filter-tech')
+const filterTechnology = document.querySelector('#filter-technology')
 
-filterSelect?.addEventListener('change', (event) => {
-  console.log(event.target.value)
+filterTechnology?.addEventListener('change', (event) => {
+  const selectedTech = event.target.value
+  console.log(selectedTech)
+
+  // Mostrar todos los trabajos si no hay una tecnología seleccionada
+  if (!selectedTech) {
+    document.querySelectorAll('.job-description').forEach(job => {
+      job.style.display = 'block'
+    })
+    return
+  }
+
+  // Ocultar los trabajos que no coinciden con la tecnología seleccionada
+  document.querySelectorAll('.job-description').forEach(job => {
+    const jobTechnologies = job.getAttribute('data-technology').split(' ')
+
+    if (jobTechnologies.includes(selectedTech)) {
+      job.style.display = 'block'
+    } else {
+      job.style.display = 'none'
+    }
+  })
+})
+
+const filterLocation = document.querySelector('#filter-location')
+
+filterLocation?.addEventListener('change', (event) => {
+  const selectLocation = event.target.value
+  console.log(selectLocation)
+
+  // Mostrar todos los trabajos si no hay una ubicación seleccionada
+  if (!selectLocation) {
+    document.querySelectorAll('.job-description').forEach(job => {
+      job.style.display = 'block'
+    })
+    return
+  }
+
+  // Ocultar los trabajos que no coinciden con la ubicación seleccionada
+  document.querySelectorAll('.job-description').forEach(job => {
+    const jobLocation = job.getAttribute('data-location')
+
+    if (jobLocation === selectLocation) {
+      job.style.display = 'block'
+    } else {
+      job.style.display = 'none'
+    }
+  })
 })
